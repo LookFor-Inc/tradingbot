591b93080845c62a3c58be674b3c841c19fae856
Oleg Chushkin <chushckin.ol@yandex.ru>
2021-03-26 19:26:50 +0300

    Upd: added request to users tickers service

--- src/main/java/com/lookfor/trading/repositories/UsersTickersRepository.java
+++ src/main/java/com/lookfor/trading/repositories/UsersTickersRepository.java
@@ -2,16 +2,11 @@ package com.lookfor.trading.repositories;
 
 import com.lookfor.trading.models.UsersTickers;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 
 @Repository
 @Transactional
 public interface UsersTickersRepository extends JpaRepository<UsersTickers, Long> {
-
-    @Query(value = "SELECT Name FROM UsersTickers", nativeQuery = true)
-    List<String> findAllNames();
 }
